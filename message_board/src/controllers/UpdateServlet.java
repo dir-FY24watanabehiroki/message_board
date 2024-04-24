5c78f9c17a21f35d0b9b687600a3f23358cade4d
HirokiWatanabe(渡辺大輝) <hiroki,watanabe.t668@dirbato.co.jp>
2024-04-24 09:24:16 +0900

    バリデーションを追加

--- message_board/src/controllers/UpdateServlet.java
+++ message_board/src/controllers/UpdateServlet.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
+import models.validators.MessageValidator;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -38,6 +42,20 @@ public class UpdateServlet extends HttpServlet {
 
             var currentTime = new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime); // 更新日時のみ上書き
+            
+         // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
+
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {
 
             // データベースを更新
             em.getTransaction().begin();
@@ -53,4 +71,5 @@ public class UpdateServlet extends HttpServlet {
         }
     }
 
+}
 }
